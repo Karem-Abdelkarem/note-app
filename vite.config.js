48c71032fd5b5fc4bb5e0c35ac40c02669ef59f7
Kareem Abdelkarem <km6263458@gmail.com>
2025-09-14 18:29:38 +0300

    fix: import resolve from path to treat both index.html and add-note.html as entry points.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/note-app",
+  base: "/note-app/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        addNote: resolve(__dirname, "add-note.html"),
+      },
+    },
+  },
 });
